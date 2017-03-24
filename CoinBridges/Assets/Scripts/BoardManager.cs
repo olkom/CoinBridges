00ee173c8a6f3fbde939e71141aed79a6cdb104f
olkom <backstrom.svante@gmail.com>
2017-03-25 00:36:06 +0100

    Boardmanager update
    Coins now instantiates from prefab, not gameobject.

--- CoinBridges/Assets/Scripts/BoardManager.cs
+++ CoinBridges/Assets/Scripts/BoardManager.cs
@@ -5,11 +5,16 @@ using Random = UnityEngine.Random;
 
 public class BoardManager : MonoBehaviour {
 
-    
-    Color[] coinColors = { Color.green, Color.red, Color.blue, Color.yellow, Color.black };
-    public int columns = 2;
-    public int rows = 2;
-    public GameObject coin;
+    //the colors of the coin sides.
+    Color[] coinColors = { Color.green, Color.red, Color.blue, Color.yellow };
+    // the columns, rows and CoinPrefab are set in the inspector.
+    public int columns; 
+    public int rows;
+    public Transform CoinPrefab;
+    public int startingX;
+    public int startingZ;
+    public int endingX;
+    public int endingZ;
 
     private Transform boardHolder;
 
@@ -25,15 +30,22 @@ public class BoardManager : MonoBehaviour {
                 //randomizing color for the coin about to be placed.
                 int botColorNr = Random.Range(0, coinColors.Length);
                 int topColorNr = Random.Range(0, coinColors.Length);
-                //instantiate the coin at the x , z coordinates that we loop through
-                GameObject instance = Instantiate(coin, new Vector3(x, 0.3f, z), Quaternion.identity) as GameObject;
+                //instantiate the coin prefab at the x , z coordinates that we loop through
+                Transform instance = Instantiate(CoinPrefab, new Vector3(x, 0.3f, z), Quaternion.identity);
                 //accessing the renderers of the child and parent (bottom and top)
                 Renderer[] renderers = instance.GetComponentsInChildren<Renderer>();
                 //accessing the bottom renderer by looking at both renderers and 
                 foreach (Renderer renderer in renderers)
                 {
                     if (renderer.gameObject.transform.parent != null) { //child renderer -> bottomcolor
-                        renderer.material.color = coinColors[botColorNr];
+                        //this sets the starting and ending coins bottom color to black.
+                        if ((x == startingX && z==startingZ) || (x == endingX && z== endingZ))
+                        {
+                            renderer.material.color = Color.black;
+                        } else
+                        {
+                            renderer.material.color = coinColors[botColorNr];
+                        }
                     } else //parent renderer -> topColor
                     {
                         renderer.material.color = coinColors[topColorNr];
